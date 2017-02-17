dfd59fa52c1e851881245e9df51dfb31b9ca9c03
02l <mitrakos12@gmail.com>
2017-02-17 09:15:46 -0800

    bug fix

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SignupService } from './signup.service';
+import { LoginService } from '../login/login.service';
 
 @Component({
     selector: 'app-signup',
@@ -11,13 +12,14 @@ export class SignupComponent implements OnInit {
 
   user: any = {};
 
-  constructor(private signUpService: SignupService) { }
+  constructor(private signUpService: SignupService, private loginService: LoginService) { }
 
   ngOnInit() {
   }
 
   submitSignup = () => {
     this.signUpService.signUp(this.user.username, this.user.password, this.user.email, this.user.firstname, this.user.lastname)
+    // this.loginService.login(this.user.username, this.user.password)
     .subscribe(res => {
       this.user.firstname = '';
       this.user.lastname = '';
